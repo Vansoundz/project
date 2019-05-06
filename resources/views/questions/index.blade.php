ba090af7778356c812a7a2b44cc976331b7f2015
vansoundz <evannivan@gmail.com>
2019-05-06 14:55:40 +0300

    Implementing authorisation using gates

--- resources/views/questions/index.blade.php
+++ resources/views/questions/index.blade.php
@@ -36,7 +36,11 @@
                                         <a href="{{ $question->url }}">{{ $question->title }}</a>
                                     </h3>
                                     <div class="ml-auto">
+                                        @if (Auth::user()->can('update-question', $question))
                                         <a href="{{ route('questions.edit', $question->id)}}" class="btn btn-sm btn-outline-info">Edit</a>
+                                        @endif
+
+                                        @if (Auth::user()->can('delete-question', $question))
                                         <form method="post" action="{{ route('questions.destroy', $question->id) }}" class="form-delete">
                                             @method('DELETE')
                                             @csrf 
@@ -44,6 +48,7 @@
                                                 Delete
                                             </button>
                                         </form>
+                                        @endif
                                     </div>
                                 </div>
                                 <p class="lead">
